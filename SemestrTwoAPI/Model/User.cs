cda22db56d653f2a0bcf62e40f8a3ae26728f032
Мурин Кирилл <murinkd@gmail.com>
2025-03-21 08:45:01 +0300

    UserModel added

---
+++ SemestrTwoAPI/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace SemestrTwoAPI.Model
+{
+    [Index(nameof(User.Email), IsUnique = true)]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]        
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Password { get; set; } 
+    }
+}
